27cf45ca32951eb1e257f8cd669c630b79a79cf2
captify-dgalea <44162052+captify-dieter@users.noreply.github.com>
2019-04-30 12:00:48 +0100

    add catboost support

--- hpsklearn/components.py
+++ hpsklearn/components.py
@@ -16,6 +16,12 @@ from hyperopt.pyll import scope, as_apply
 from hyperopt import hp
 from .vkmeans import ColumnKMeans
 from . import lagselectors
+
+try:
+    import catboost
+except ImportError:
+    catboost = None
+
 # Optional dependencies
 try:
     import xgboost
@@ -106,6 +112,12 @@ def sklearn_XGBClassifier(*args, **kwargs):
     if xgboost is None:
         raise ImportError('No module named xgboost')
     return xgboost.XGBClassifier(*args, **kwargs)
+
+@scope.define
+def CatBoostClassifier(*args, **kwargs):
+    if catboost is None:
+        raise ImportError('No module named catboost')
+    return catboost.CatBoostClassifier(*args, **kwargs)
 
 @scope.define
 def sklearn_XGBRegressor(*args, **kwargs):
@@ -1363,6 +1375,172 @@ def xgboost_regression(name, objective='reg:linear', **kwargs):
     hp_space = _xgboost_hp_space(_name, **kwargs)
     hp_space['objective'] = objective
     return scope.sklearn_XGBRegressor(**hp_space)
+
+###################################################
+##==== CatBoost hyperparameters search space ====##
+###################################################
+
+def _catboost_max_depth(name):
+    "Recommended to be optimized. Maximum is 16 vs. XGBoost"
+    return scope.int(hp.uniform(name, 1, 16))
+
+def _catboost_learning_rate(name):
+    "Recommended to be optimized"
+    return hp.loguniform(name, np.log(0.0001), np.log(0.5)) - 0.0001
+
+def _catboost_n_estimators(name):
+    "Recommended to be optimized"
+    return scope.int(hp.quniform(name, 100, 6000, 200))
+
+def _catboost_subsample(name):
+    return hp.uniform(name, 0.5, 1)
+
+def _catboost_colsample_bytree(name):
+    return hp.uniform(name, 0.5, 1)
+
+def _catboost_colsample_bylevel(name):
+    return hp.uniform(name, 0.5, 1)
+
+def _catboost_reg_alpha(name):
+    return hp.loguniform(name, np.log(0.0001), np.log(1)) - 0.0001
+
+def _catboost_reg_lambda(name):
+    "Recommended to be optimized. Alias: l2_leaf_reg"
+    return hp.loguniform(name, np.log(1), np.log(4))
+
+def _catboost_bagging_temp(name):
+    "Recommended to be optimized."
+    return hp.uniform(name, 0, 100000)
+
+def _catboost_random_strength(name):
+    return hp.uniform(name, 0, 1)
+
+def _catboost_scale_pos_weight(name):
+    return hp.uniform(name, 0.1, 10)
+
+def _catboost_hp_space(name_func,
+    iterations=None,
+    learning_rate=None,
+    depth=None,
+    l2_leaf_reg=None,
+    model_size_reg=None,
+    rsm=None,
+    loss_function='Logloss',
+    border_count=None,
+    feature_border_type=None,
+    input_borders=None,
+    output_borders=None,
+    fold_permutation_block=None,
+    od_pval=None,
+    od_wait=None,
+    od_type=None,
+    nan_mode=None,
+    counter_calc_method=None,
+    leaf_estimation_iterations=None,
+    leaf_estimation_method=None,
+    thread_count=None,
+    random_seed=None,
+    use_best_model=None,
+    verbose=None,
+    logging_level=None,
+    metric_period=None,
+    ctr_leaf_count_limit=None,
+    store_all_simple_ctr=None,
+    max_ctr_complexity=None,
+    has_time=None,
+    allow_const_label=None,
+    classes_count=None,
+    class_weights=None,
+    one_hot_max_size=None,
+    random_strength=None,
+    name=None,
+    ignored_features=None,
+    train_dir=None,
+    custom_loss=None,
+    custom_metric=None,
+    eval_metric=None,
+    bagging_temperature=None,
+    save_snapshot=None,
+    snapshot_file=None,
+    snapshot_interval=None,
+    fold_len_multiplier=None,
+    used_ram_limit=None,
+    gpu_ram_part=None,
+    allow_writing_files=None,
+    final_ctr_computation_mode=None,
+    approx_on_full_history=None,
+    boosting_type=None,
+    simple_ctr=None,
+    combinations_ctr=None,
+    per_feature_ctr=None,
+    task_type=None,
+    device_config=None,
+    devices=None,
+    bootstrap_type=None,
+    subsample=None,
+    sampling_unit=None,
+    dev_score_calc_obj_block_size=None,
+    max_depth=None,
+    n_estimators=None,
+    num_boost_round=None,
+    num_trees=None,
+    colsample_bylevel=None,
+    random_state=None,
+    reg_lambda=None,
+    objective=None,
+    eta=None,
+    max_bin=None,
+    scale_pos_weight=None,
+    gpu_cat_features_storage=None,
+    data_partition=None,
+    metadata = None,
+    early_stopping_rounds = None,
+    cat_features = None,
+    grow_policy = None,
+    min_data_in_leaf = None,
+    max_leaves = None,
+    score_function = None,
+    leaf_estimation_backtracking = None,
+    n_jobs=-1):
+    '''Generate CatBoost hyperparameters search space'''
+    hp_space = dict(
+        max_depth=(_catboost_max_depth(name_func('max_depth'))
+                   if max_depth is None else max_depth),
+        learning_rate=(_catboost_learning_rate(name_func('learning_rate'))
+                       if learning_rate is None else learning_rate),
+        n_estimators=(_catboost_n_estimators(name_func('n_estimators'))
+                      if n_estimators is None else n_estimators),
+        # subsample=(_catboost_subsample(name_func('subsample'))
+        #            if subsample is None else subsample),
+        # colsample_bylevel=(_catboost_colsample_bylevel(name_func('colsample_bylevel'))
+        #                   if colsample_bylevel is None else colsample_bylevel),
+        reg_lambda=(_catboost_reg_lambda(name_func('reg_lambda'))
+                    if reg_lambda is None else reg_lambda),
+        scale_pos_weight=(_catboost_scale_pos_weight(name_func('scale_pos_weight'))
+                    if scale_pos_weight is None else scale_pos_weight),
+        bagging_temperature=(_catboost_bagging_temp(name_func('bagging_temperature'))
+                             if bagging_temperature is None else bagging_temperature),
+        random_strength=(_catboost_random_strength(name_func('random_strength'))
+                             if random_strength is None else random_strength),
+        seed=_random_state(name_func('rstate'), random_state),
+        n_jobs=n_jobs
+    )
+    return hp_space
+
+
+########################################################
+##==== CatBoost classifier/regressor constructors ====##
+########################################################
+def catboost_classification(name, **kwargs):
+    '''
+    Return a pyll graph with hyperparameters that will construct
+    a catboost.CatBoostClassifier model.
+    '''
+    def _name(msg):
+        return '%s.%s_%s' % (name, 'catboost', msg)
+
+    hp_space = _catboost_hp_space(_name, **kwargs)
+    return scope.CatBoostClassifier(**hp_space)
 
 
 #################################################
